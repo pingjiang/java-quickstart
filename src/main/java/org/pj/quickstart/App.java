ac41b9135bea1e95d2d112619da5d8b97675832f
pingjiang <pingjiang1989@gmail.com>
2014-09-25 13:25:49 +0800

    Initial Commit

---
+++ src/main/java/org/pj/quickstart/App.java
@@ -0,0 +1,13 @@
+package org.pj.quickstart;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/org/pj/quickstart/AppTest.java
@@ -0,0 +1,19 @@
+package org.pj.quickstart;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
